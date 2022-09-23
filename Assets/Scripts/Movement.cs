4b567bf51e1d3deef3727bcf34fa8b87b6771f4c
THMKruemel <lennartsigge@icloud.com>
2022-09-23 19:11:02 +0200

    movement script delete

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveInputx;
-    public float moveInputy;
-    public int moveSpeed;
+   
+
+    private void Start()
+    {
+   
+    }
     void Update()
     {
         Moving();
     }
     void Moving()
     {
-
-        moveInputx = Input.GetAxisRaw("Horizontal");
-        moveInputy = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(moveInputx * moveSpeed, rb.velocity.x);
-        rb.velocity = new Vector2(moveInputy * moveSpeed, rb.velocity.y);
-        
 
     }
 
