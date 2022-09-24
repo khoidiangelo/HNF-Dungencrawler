919e54bb7bce087aa2a1beceee2ea8f5ec9dbad9
THMKruemel <lennartsigge@icloud.com>
2022-09-24 11:58:30 +0200

    add chest script

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class ChestScript : MonoBehaviour
 {
-   
+    
+
+    private void Start()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Globals.healPotion += 1;
+        Destroy(gameObject);
+    }
 }
