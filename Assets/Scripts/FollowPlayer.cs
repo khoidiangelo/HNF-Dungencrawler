0f8dbc30ee4af6e229dddbe4d233bf6567db7d87
Khoi <timhorochowik@icloud.com>
2022-09-24 21:11:58 +0200

    fixed cedrics shit camera

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -12,12 +12,16 @@ public class FollowPlayer : MonoBehaviour
     public float Max_Offset;
     public float speed;
 
+    public bool doShit = false;
+
     private void Start()
     {
         offset = transform.position - target.transform.position;
     }
     void Update()
     {
+        if (doShit == true)
+        {
         Vector3 current_target = target.position + offset + (new Vector3(Movement.Instance.Direction.x, Movement.Instance.Direction.y, 0))* 2f;
         Mathf.Clamp(current_target.x, target.position.x - Max_Offset, target.position.x + Max_Offset);
         Mathf.Clamp(current_target.y, target.position.y - Max_Offset, target.position.y + Max_Offset);
@@ -25,6 +29,12 @@ public class FollowPlayer : MonoBehaviour
         //Vector3 current_target = target.position + offset;
         var step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, current_target, step);
+        }
+        else
+        {
+            transform.position = target.transform.position + offset;
+        }
+        
 
 
     }
