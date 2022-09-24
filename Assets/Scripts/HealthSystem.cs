912d4b1202263350ea61f8d5e4e148bcbea18e80
Khoi <timhorochowik@icloud.com>
2022-09-24 15:48:33 +0200

    fuck you cedric

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    
-   
-   
-    public int healPotion;
-    public Image[] hearts;
-    public Sprite emptyHeart;
-    public Sprite fullHeart;
+    public static HealthSystem instance;
 
 
     public int max_Health;
@@ -32,7 +26,6 @@ public class HealthSystem : MonoBehaviour
     }
     private void Start()
     {
- 
         current_Health = max_Health;
 
     }
@@ -42,16 +35,6 @@ public class HealthSystem : MonoBehaviour
         check_HealPotion();
 
         if (invinceileCount >= 0)
-
-        if(Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            Globals.playerHealth -= 1;
-        }
-        
-        HealPotion();
-
-        if (Globals.playerHealth > Globals.numberOfHearts)
- 
         {
             invinceileCount -= Time.deltaTime;
         }
@@ -59,65 +42,14 @@ public class HealthSystem : MonoBehaviour
         
         if (current_Health <= 0)
         {
- 
             GameManager.instance.has_died();
-
-            if (i < Globals.playerHealth)
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].sprite = fullHeart;
-                }
-
-
-            }
-            else
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].sprite = emptyHeart;
-                }
-
-            }
-            if (i < Globals.numberOfHearts)
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].enabled = true;
-                }
-
-
-            }
-            else
-            {
-                if (i < Globals.numberOfHearts)
-                {
-                    hearts[i].enabled = false;
-                }
-
-            }
-
         }
-        if (Globals.playerHealth <= 0)
-        {
-            Globals.dead = 1;
-            if(Globals.dead == 1)
-            {
-                Globals.healPotion = 0;
-                Time.timeScale = 0;
-                deadPanel.SetActive(true);
-
-            }
- 
-        }
-        
     }
 
 
 
     public void DamagePlayer(int damageAmount)
     {
- 
         if (invinceileCount <= 0)
         {
             current_Health -= damageAmount;
@@ -142,9 +74,6 @@ public class HealthSystem : MonoBehaviour
     {
 
         if (Input.GetKeyDown(KeyCode.H))
-
-        if (Input.GetKeyDown(KeyCode.H) && Globals.healPotion > 0 && Globals.playerHealth != 3)
- 
         {
             this.DamagePlayer(-1);
         }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,28 +9,45 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
-    internal Animator animator;
-   
-    void Awake()
+    public Vector2 Direction;
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        Instance = this;
     }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("horizontal speed", movement.x);
-        animator.SetFloat("vertical speed", movement.y);
-
-
+        if (movement.sqrMagnitude > 0)
+        {
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                if (movement.x > 0)
+                {
+                    Direction = new Vector2(1f, 0f);
+                }
+                else
+                {
+                    Direction = new Vector2(-1f, 0f);
+                }
+            }
+            else 
+            {
+                if (movement.y > 0)
+                {
+                    Direction = new Vector2(0f, 1f);
+                }
+                else
+                {
+                    Direction = new Vector2(0f, -1f);
+                }
+            }
+        }
     }
-    
 
     void FixedUpdate()
     {
-       
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
