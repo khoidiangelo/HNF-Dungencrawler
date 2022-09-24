a3314bdc29d837934324667407140a065b506907
Lazarus159 <ceddi.sch@gmail.com>
2022-09-24 15:20:52 +0200

    finally

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,100 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class HealthSystem : MonoBehaviour
 {
-    public int health;
-    public int maxHealth;
-    public int numberOfHearts;
-    public int healPotion;
-    public Image[] hearts;
-    public Sprite emptyHeart;
-    public Sprite fullHeart;
+    public static HealthSystem instance;
 
-    public GameObject deadPanel;
-    
+
+    public int max_Health;
+    public float invincibleTime = 0.5f;
+
+    public int numberOfHearts;
+
+    public int current_Health;
+
+    public int storred_healPotions;
+
+    public float invinceileCount;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
     private void Start()
     {
-        health = maxHealth;
+        current_Health = max_Health;
+
     }
 
     void Update()
     {
-        HealPotion();
-        if (Globals.playerHealth > Globals.numberOfHearts)
+        check_HealPotion();
+
+        if (invinceileCount >= 0)
         {
-            Globals.playerHealth = Globals.numberOfHearts;
+            invinceileCount -= Time.deltaTime;
         }
-        for (int i = 0; i < hearts.Length; i++)
+
+        
+        if (current_Health <= 0)
         {
-            if (i < Globals.playerHealth)
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].sprite = fullHeart;
-                }
-
-
-            }
-            else
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].sprite = emptyHeart;
-                }
-
-            }
-            if (i < Globals.numberOfHearts)
-            {
-                if (hearts[i] != null)
-                {
-                    hearts[i].enabled = true;
-                }
-
-
-            }
-            else
-            {
-                if (i < Globals.numberOfHearts)
-                {
-                    hearts[i].enabled = false;
-                }
-
-            }
-
-        }
-        if(Globals.playerHealth <= 0)
-        {
-            Globals.dead = 1;
-            if(Globals.dead == 1)
-            {
-                Time.timeScale = 1;
-                deadPanel.SetActive(true);
-            }
+            GameManager.instance.has_died();
         }
     }
-    void HealPotion()
+
+
+
+    public void DamagePlayer(int damageAmount)
     {
+        if (invinceileCount <= 0)
+        {
+            current_Health -= damageAmount;
+
+            Ui_Controler.instance.render_take_Damage();
+
+            if (current_Health <= 0)
+            {
+                gameObject.SetActive(false);
+
+                current_Health = 0;
+
+                GameManager.instance.has_died();
+            }
+
+            invinceileCount = invincibleTime;
+        }
+
+        Ui_Controler.instance.render_Hearts(current_Health);
+    }
+    void check_HealPotion()
+    {
+
         if (Input.GetKeyDown(KeyCode.H))
         {
-            Globals.playerHealth += 1;
-            Globals.healPotion -= 1;
-        }
-
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("enemyAttack"))
-        {
-            Globals.playerHealth -= 1;
-        }
-        if(collision.gameObject.CompareTag("border"))
-        {
-            Globals.playerHealth -= 3;
+            this.DamagePlayer(-1);
         }
     }
+   
     
 
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public static Movement Instance;
     public float moveSpeed = 5;
     public Rigidbody2D rb;
 
     Vector2 movement;
+    public Vector2 Direction;
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.sqrMagnitude > 0)
+        {
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                if (movement.x > 0)
+                {
+                    Direction = new Vector2(1f, 0f);
+                }
+                else
+                {
+                    Direction = new Vector2(-1f, 0f);
+                }
+            }
+            else 
+            {
+                if (movement.y > 0)
+                {
+                    Direction = new Vector2(0f, 1f);
+                }
+                else
+                {
+                    Direction = new Vector2(0f, -1f);
+                }
+            }
+        }
     }
 
     void FixedUpdate()
